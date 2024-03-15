490ee0c255c6c5457f175707e3e435f69148313f
Tony <tony.w.trinh3@gmail.com>
2024-03-15 16:34:25 -0400

    deleted unused imports and commented out unused functions in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,41 @@
 //@ts-nocheck
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { AnimatePresence, motion } from "framer-motion";
-import { ArrowRight, Globe } from "lucide-react";
-import { useRouter } from "next/router";
-import { CSNav } from "@/components/ui/csnav";
-import NavBar from "@/components/ui/navbar";
 import Script from "next/script";
 import Airways from "./airways";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  const router = useRouter();
 
-  const goToBank = () => {
-    router.push("/bank");
-  };
+  //TODO: delete after if we make sure if we aren't using this to develop anything 
+  // const router = useRouter();
 
-  const goToNext = () => {
-    router.push("/devs");
-  };
+  // const variants = {
+  //   hidden: { opacity: 0 },
+  //   show: {
+  //     opacity: 1,
+  //     transition: {
+  //       staggerChildren: 0.5, // This will create the staggered effect
+  //     },
+  //   },
+  // };
 
-  const variants = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.5, // This will create the staggered effect
-      },
-    },
-  };
+  // const childVariants = {
+  //   hidden: { opacity: 0 },
+  //   show: { opacity: 1 },
+  // };
 
-  const childVariants = {
-    hidden: { opacity: 0 },
-    show: { opacity: 1 },
-  };
+  // const pageVariants = {
+  //   initial: { x: 0 },
+  //   in: { x: 0 },
+  //   out: { x: "-100%" },
+  // };
 
-  const pageVariants = {
-    initial: { x: 0 },
-    in: { x: 0 },
-    out: { x: "-100%" },
-  };
-
-  const pageTransition = {
-    type: "tween",
-    ease: "anticipate",
-    duration: 0.5,
-  };
+  // const pageTransition = {
+  //   type: "tween",
+  //   ease: "anticipate",
+  //   duration: 0.5,
+  // };
 
   return (
     <>
-      {/* <!-- Google tag (gtag.js) --> */}
-      <Script
-        strategy="lazyOnload"
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-2ZW2MJ75NL"
-      ></Script>
-      <Script strategy="lazyOnload">
-        {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-2ZW2MJ75NL');`}
-      </Script>
       <Airways/>
     </>
   );
